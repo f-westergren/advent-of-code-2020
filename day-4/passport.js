f7263e9b7f8e04b759df653e088193b97fabfcbf
f-westergren <folke.westergren@gmail.com>
2020-12-06 20:27:17 -0500

    Day 4 part 2

--- day-4/passport.js
+++ day-4/passport.js
@@ -29,7 +29,30 @@ lineReader.on("close", () => {
     let passLen = Object.keys(passport).length;
 
     if (passLen === 8 || (passLen === 7 && !passport.hasOwnProperty("cid"))) {
-      sum++;
+      // Part 2 validation:
+      let valid = true;
+      let { byr, iyr, eyr, hgt, hcl, ecl, pid, cid } = passport;
+      if (+byr < 1920 || +byr > 2002) valid = false;
+      if (+iyr < 2010 || +iyr > 2020) valid = false;
+      if (+eyr < 2020 || +eyr > 2030) valid = false;
+      if (!hgt.toString().match(/cm/) && !hgt.toString().match(/in/))
+        valid = false;
+      if (hgt.toString().match(/cm/)) {
+        hgt = +hgt.slice(0, hgt.length - 2);
+        if (hgt < 150 || hgt > 193) valid = false;
+      }
+      if (hgt.toString().match(/in/)) {
+        hgt = +hgt.slice(0, hgt.length - 2);
+        if (hgt < 59 || hgt > 76) valid = false;
+      }
+      if (hcl[0] !== "#" || hcl.slice(1).length !== 6) valid = false;
+      if (!["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(ecl))
+        valid = false;
+      if (pid.length !== 9) valid = false;
+
+      if (valid) {
+        sum++;
+      }
     }
   });
   console.log(sum);
