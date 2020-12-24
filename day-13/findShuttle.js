32c030530b9b8f4932baa01b71b71589945e7660
f-westergren <folke.westergren@gmail.com>
2020-12-24 10:41:54 -0500

    Day 13, part 2, doesn't work yet

--- day-13/findShuttle.js
+++ day-13/findShuttle.js
@@ -1,20 +1,43 @@
-let departureTime = 1000053
-let buses = '19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,23,x,x,x,x,x,29,x,547,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,17';
+let departureTime = 1000053;
+let buses =
+  "19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,23,x,x,x,x,x,29,x,547,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,17";
 
-let busList = buses.split(',').filter(bus => bus !== 'x').map(bus => +bus)
+// Part 1
+let busList = buses
+  .split(",")
+  .filter((bus) => bus !== "x")
+  .map((bus) => +bus);
 
 const checkDeparture = () => {
-  let waitTime = undefined;
   let busNumber;
-  busList.forEach(bus => {
-      let minutes = (bus * (Math.floor(departureTime / bus)) + bus) - departureTime
-      if (minutes < waitTime || waitTime === undefined) {
-        waitTime = minutes;
-        busNumber = bus
-      }
-    })
+  let waitTime = undefined;
+  busList.forEach((bus) => {
+    let minutes = bus * Math.floor(departureTime / bus) + bus - departureTime;
+    if (minutes < waitTime || !waitTime) {
+      waitTime = minutes;
+      busNumber = bus;
+    }
+  });
+  console.log("Answer Part 1:", waitTime * busNumber);
+};
 
-  console.log('Answer Part 1:', waitTime * busNumber)
+// Part 2
+let timeTable = {};
+let timeTableList = buses.split(",");
+
+for (let i = 1; i < timeTableList.length; i++) {
+  if (timeTableList[i] !== "x") timeTable[i] = +timeTableList[i];
 }
 
-checkDeparture()
+let check = false;
+let i = 1;
+
+while (!check) {
+  check = true;
+  i++;
+  let firstDep = i * +timeTableList[0];
+  check = !Object.keys(timeTable).some(
+    (x) => ((firstDep + +x) / timeTable[x]) % 1 !== 0
+  );
+  console.log(firstDep, check);
+}
