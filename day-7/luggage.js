08db56b36dfe98baabf0b579c02a608af8216d79
f-westergren <folke.westergren@gmail.com>
2020-12-28 15:19:59 -0500

    Day 7, part 2

--- day-7/luggage.js
+++ day-7/luggage.js
@@ -4,12 +4,15 @@ const lineReader = require("readline").createInterface({
 
 const luggage = [];
 let bagsList = [];
+let goldBagContent = [];
+
 lineReader.on("line", (line) => {
   if (line.indexOf(" shiny gold") !== -1)
     bagsList.push(line.split(" bags contain")[0]);
   luggage.push(line);
 });
 
+// Part 1
 const findBags = (bags) => {
   let newBags = [];
   bags.forEach((bag) => {
@@ -25,7 +28,55 @@ const findBags = (bags) => {
   }
 };
 
+// Part 2
+const countBags = (line) => {
+  let newBags = [];
+
+  if (line.match(/no\sother/g)) return newBags;
+
+  let regex = /\d\s/g;
+  let bags = line
+    .replace(/\sbags,\s|\sbags.|\sbag,\s|\sbag./g, "")
+    .split(regex)
+    .slice(1);
+  let nums = line.match(regex);
+
+  bags.forEach((bag, idx) => {
+    for (let i = 0; i < nums[idx]; i++) {
+      newBags.push(bag);
+    }
+  });
+  return newBags;
+};
+
+const findBag = (bag) => {
+  let check = false;
+  let i = -1;
+  while (!check) {
+    i++;
+    if (luggage[i].startsWith(bag)) check = true;
+  }
+  return luggage[i];
+};
+
+const checkBags = (bags) => {
+  let newBags = [];
+  bags.forEach((bag) => {
+    let line = findBag(bag);
+    newBags.push(...countBags(line));
+  });
+  if (newBags.length > 0) {
+    goldBagContent.push(...newBags);
+    checkBags(newBags);
+  }
+};
+
 lineReader.on("close", () => {
+  // Part 1
   findBags(bagsList);
   console.log("Part 1:", [...new Set(bagsList)].length);
+
+  // Part 2
+  checkBags(["shiny gold"]);
+  console.log("Part 2:", goldBagContent.length);
 });
