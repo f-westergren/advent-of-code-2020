10730ab929f4a4eced8a0a5bafe9b3d1b18f4e41
f-westergren <folke.westergren@gmail.com>
2021-01-02 12:41:14 -0500

    Day 10, Part 1

---
+++ day-10/adapters.js
@@ -0,0 +1,30 @@
+const lineReader = require("readline").createInterface({
+  input: require("fs").createReadStream("input.txt"),
+});
+
+const adapters = [];
+
+lineReader.on("line", (line) => {
+  adapters.push(+line);
+});
+
+const findJumps = (array) => {
+  let ones = 0;
+  let threes = 0;
+
+  // Add charging outlet input (always 0)
+  array.push(0);
+  array.sort((a, b) => a - b);
+
+  // Add device built-in adapter (always 3 jolts higher than highest adapter)
+  array.push(array[array.length - 1] + 3);
+
+  for (let i = 0; i < array.length - 1; i++) {
+    array[i + 1] - array[i] === 1 ? ones++ : threes++;
+  }
+  return ones * threes;
+};
+
+lineReader.on("close", () => {
+  console.log(findJumps(adapters));
+});
